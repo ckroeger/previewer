0c248ad791639f86f69dbcfe2540726d6502b170
ckroeger <ckroeger.hh@gmail.com>
2023-11-20 00:07:07 +0100

    COPY from origin

--- src/main_copy.go
+++ src/main_copy.go
@@ -7,7 +7,6 @@ import (
 	"image/jpeg"
 	"io/fs"
 	"log"
-	"math"
 	"os"
 	"path/filepath"
 	"strings"
@@ -35,20 +34,18 @@ var (
 	srcDir      string
 	templateDir string
 	outDir      string
-	maxWidth    int
-	maxHeight   int
 )
 
 func initFlags() {
 	flag.StringVar(&srcDir, "src", "", "Directory containing source files")
 	flag.StringVar(&templateDir, "templates", "", "Directory containing template files to render")
 	flag.StringVar(&outDir, "outDir", "", "Resized image target directory.")
-	flag.IntVar(&maxWidth, "maxWidth", 0, "maxWidth")
-	flag.IntVar(&maxHeight, "maxHeight", 0, "maxHeight")
 	flag.Parse()
 	if outDir == "" {
 		outDir = srcDir
 	}
+	log.Println("OUTDIR: ", outDir)
+	log.Println("SRCDIR: ", srcDir)
 }
 
 func main() {
@@ -101,9 +98,6 @@ func renderTemplates(tpls []Tpl, imgs []Image) {
 	}
 }
 
-func isJpg(name string) bool {
-	return strings.HasSuffix(name, "jpg") || strings.HasSuffix(name, "jpeg")
-}
 func findImages(srcDir string) []Img {
 	var items []Img
 
@@ -111,9 +105,10 @@ func findImages(srcDir string) []Img {
 		if err != nil {
 			return err
 		}
-		if !d.IsDir() && !strings.Contains(d.Name(), "__th__") && isJpg(d.Name()) {
+		if !d.IsDir() && !strings.Contains(d.Name(), "__th__") {
 			relPath := strings.Replace(path, srcDir+"/", "", 1)
 			relDir := strings.Replace(relPath, "/"+d.Name(), "", 1)
+			log.Printf("-> %v", d.Name())
 			items = append(items, Img{AbsPath: path, RelPath: relDir, Filename: d.Name()})
 		}
 		return nil
@@ -127,29 +122,13 @@ func findImages(srcDir string) []Img {
 func resizeImages(items []Img, targetPath string) []Image {
 	var resizedImg []Image
 	for _, item := range items {
-		newFilename, absPathOut := resizeImage(item, targetPath)
+		// maxWidth maxHeigth
 
+		newFilename, absPathOut := resizeImage(item, targetPath)
+		log.Printf("targetPath -> %v", targetPath)
 		resizedImg = append(resizedImg, Image{Origin: item, Resized: Img{AbsPath: absPathOut, RelPath: item.RelPath, Filename: newFilename}})
 	}
 	return resizedImg
-}
-func scale(oldWidth int, oldHeight int, maxWidth int, maxHeight int) (width int, height int) {
-	if maxWidth == 0 && maxHeight == 0 {
-		maxWidth = oldWidth / 2
-		maxHeight = oldHeight / 2
-	}
-	if maxWidth == 0 {
-		maxWidth = math.MaxInt
-	}
-	if maxHeight == 0 {
-		maxHeight = math.MaxInt
-	}
-	ratioX := float32(maxWidth) / float32(oldWidth)
-	ratioY := float32(maxHeight) / float32(oldHeight)
-	ratio := min(ratioX, ratioY)
-	width = int(float32(oldWidth) * ratio)
-	height = int(float32(oldHeight) * ratio)
-	return width, height
 }
 
 func resizeImage(item Img, targetPath string) (string, string) {
@@ -159,15 +138,16 @@ func resizeImage(item Img, targetPath string) (string, string) {
 	}
 	defer input.Close()
 
-	targetDir := fmt.Sprintf("%s/%s", targetPath, item.RelPath)
-	if err := os.MkdirAll(targetDir, 0777); err != nil {
-		log.Println(err)
-	}
-	log.Printf(targetDir)
-
+	/*
+		targetDir := fmt.Sprintf("%s/%s", targetPath, item.RelPath)
+		if err := os.MkdirAll(targetDir, 0777); err != nil {
+			log.Println(err)
+		}
+		log.Printf(targetDir)
+	*/
 	newFilename := strings.Replace(item.Filename, ".jpg", "__th__.jpg", 1)
 	log.Println(newFilename)
-	absPathOut := fmt.Sprintf("%s/%s", targetDir, newFilename)
+	absPathOut := fmt.Sprintf("%s/%s", targetPath, newFilename)
 	output, err := os.Create(absPathOut)
 	if err != nil {
 		log.Println(err)
@@ -179,8 +159,7 @@ func resizeImage(item Img, targetPath string) (string, string) {
 		log.Printf("image is not a jpeg, %v", err)
 	}
 
-	width, height := scale(src.Bounds().Dx(), src.Bounds().Dy(), maxHeight, maxWidth)
-	dst := image.NewRGBA(image.Rect(0, 0, width, height))
+	dst := image.NewRGBA(image.Rect(0, 0, 160, 160))
 
 	draw.ApproxBiLinear.Scale(dst, dst.Rect, src, src.Bounds(), draw.Over, nil)
 
